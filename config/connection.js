71cdf4a234fcec3b95c47103d149a813c41ad212
Zach <zachary.fey@gmail.com>
2019-05-12 15:24:14 -0400

    adding jawsdb functionality

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 mysql = require('mysql')
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'zachary',
-    database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'zachary',
+        database: 'burgers_db'
+    })
+}
 
 module.exports = connection;
